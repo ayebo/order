86af75223be48b92113d34adec6557d63c2f20b9
NangAyeBoKham <nangayebokham@gmail.com>
2021-11-27 22:46:56 +0630

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="header">
+    <NuxtLink to="/">
+      <h1 class="headerTitle">Sharon Nang</h1>
+    </NuxtLink>
+    <NuxtLink to="/order">
+      <div class="navButton navButton--glow">Order Now</div>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  font-family: Outfit;
+  align-items: center;
+  box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
+  padding: 20px;
+  height: 120px;
+}
+.headerTitle {
+  font-weight: 200;
+  text-align: center;
+  margin: 0px;
+  color: rgb(30, 12, 112);
+}
+
+.navButton {
+  min-width: 220px;
+  padding: 18px 25px;
+  cursor: pointer;
+  position: relative;
+  font-weight: 700;
+  background: black;
+  color: #fff;
+}
+
+.navButton--glow {
+  z-index: 1;
+  transition: all 0.3s ease-out;
+  border-radius: 10px;
+}
+
+.navButton--glow:before {
+  content: '';
+  background: linear-gradient(
+    45deg,
+    #ff3c3c,
+    #4968ff,
+    #a34dff,
+    #ff53da,
+    #ff4444
+  );
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-size: 400%;
+  z-index: -1;
+  animation: glowing 20s linear infinite;
+  opacity: 0;
+  transition: all 0.3s ease-out;
+  border-radius: 10px;
+  transform-origin: center;
+  transform: translateZ(0);
+  width: 100%;
+  height: 100%;
+}
+
+.navButton--glow:hover {
+  transform: scale(1.025) translateZ(0);
+}
+.navButton--glow:hover:before {
+  opacity: 1;
+  transform: translateZ(0) scaleX(1.045) scaleY(1.145);
+}
+.navButton--glow:active::after {
+  background: rgb(36, 36, 36);
+}
+
+.navButton--glow:after {
+  z-index: -1;
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: black;
+  left: 0;
+  top: 0;
+  border-radius: 8px;
+}
+@media (max-width: 500px) {
+  .headerTitle {
+    margin-bottom: 20px;
+  }
+  .header {
+    flex-direction: column;
+    height: unset;
+  }
+}
+</style>
+
---
+++ components/Splash.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="splashBody">
+    <div>
+      <h1 class="hero">Food ordering just got a whole lot easier.</h1>
+      <p class="heroText">
+        This app was built with VueJS by Nang Aye Bo Kham. The app allows user to add thier preferable items into the cart and automatically provide the bill. The idea is adopted by the food ordering assest from Mc Donald's, etc which helps customers to order their own items without the waiters. This way, the user will get high tech enviroment just dining in the restaurant or any order related stores.
+      </p>
+    </div>
+    <img src="../assets/splash.svg" class="splashImage" alt="splash" />
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  max-width: 600px;
+  font-family: Outfit;
+}
+.splashBody {
+  display: flex;
+  gap: 40px;
+  align-items: center;
+  justify-content: center;
+  font-size: 40px;
+  max-width: 1440px;
+  padding: 20px;
+  min-height: calc(100vh - 120px);
+  margin: 0px;
+  padding-bottom: 40px;
+}
+.heroText {
+  font-family: Inter;
+  font-size: 24px;
+  font-weight: 200;
+  line-height: 1.5em;
+}
+.splashImage {
+  height: 400px;
+}
+@media (max-width: 1200px) {
+  .splashImage {
+    height: 300px;
+  }
+}
+@media (max-width: 900px) {
+  .splashImage {
+    height: unset;
+    width: 100%;
+  }
+  .splashBody {
+    flex-direction: column;
+  }
+}
+</style>
